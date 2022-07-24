e97c3702972c1f9e092c462e51292d7930fcc7aa
Carlos Pamplona <carlos.pamplona.filho@ccc.ufcg.edu.br>
2022-07-23 23:16:02 -0300

    Aula 2

---
+++ RocketHelp/src/screens/SignIn.tsx
@@ -0,0 +1,47 @@
+import {useState} from 'react'
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import Logo from "../assets/logo_primary.svg";
+
+import { Input } from "../components/Input";
+import { Envelope, Key } from "phosphor-react-native";
+import { Button } from "../components/Button";
+
+export function SignIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log(email, password);
+  }
+
+  return (
+    <VStack flex={1} alignItems="center" bg={"gray.600"} p={8} pt={24}>
+      <Logo />
+
+      <Heading color={"gray.100"} fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+          onChangeText = {setEmail}
+      />
+
+      <Input
+        placeholder="Senha"
+        mb={8}
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        onChangeText = {setPassword}
+        secureTextEntry
+      />
+
+      <Button title="Entrar" w="full" onPress={handleSignIn}/>
+    </VStack>
+  );
+}
